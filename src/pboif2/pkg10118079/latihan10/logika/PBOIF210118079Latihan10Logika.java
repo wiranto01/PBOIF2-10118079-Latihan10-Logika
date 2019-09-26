99dfe0fc51c6d343ca10244a52e02ae90e5a5f7a
wiranto01 <55836685+wiranto01@users.noreply.github.com>
2019-09-26 18:27:11 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118079/latihan10/logika/PBOIF210118079Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118079.latihan10.logika;
+
+/**
+ *
+ * @author
+ * Nama : Wiranto
+ * Kelas : If2
+ * Nim : 10118079
+ */
+public class PBOIF210118079Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+
+    }
+    
+}
